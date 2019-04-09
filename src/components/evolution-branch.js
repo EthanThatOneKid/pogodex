d8d307889b4e5b21819d5a7d9278fbe694c5786c
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-04-09 08:48:02 -0700

    Update evolution-branch.js

--- src/components/evolution-branch.js
+++ src/components/evolution-branch.js
@@ -10,8 +10,28 @@ export default class EvolutionBranch extends Component {
   }
 
   render() {
-    return (
-      <p>{JSON.stringify(this.data)}</p>
-    );
+    return this.data
+      .map(evo => {
+        const possibilities =  [
+          ["Candy Cost",     "candyCost",                  evo.hasOwnProperty("candyCost")],
+          ["Buddy Distance", "kmBuddyDistanceRequirement", evo.hasOwnProperty("kmBuddyDistanceRequirement")],
+          ["Evolution Item", "evolutionItemRequirement",   evo.hasOwnProperty("evolutionItemRequirement")]
+        ];
+        return (
+          <p>
+            {
+              possibilities
+                .filter(p => p[2])
+                .map(([displayName, key]) => {
+                  const src = `https://img.shields.io/badge/${displayName}-${this.data[key]}-green.svg`;
+                  return <img src={src}></img>;
+                });
+            }
+            →
+            {this.data.evolution}
+          </p>
+        );
+      );
   }
+
 }
