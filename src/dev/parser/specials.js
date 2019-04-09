c5564ea218220bd2ce7fb56958373e05387457f8
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-04-09 12:00:24 -0700

    Create specials.js

---
+++ src/dev/parser/specials.js
@@ -0,0 +1,24 @@
+const specials = [
+  "20180120_pikachu.json",
+  "20180224_dratini.json",
+  "20180325_bulbasaur.json",
+  "20180415_mareep.json",
+  "20180519_charmander.json",
+  "20180616_larvitar.json",
+  "20180708_squirtle.json",
+  "20180721_zapdos.json",
+  "20180728_articuno.json",
+  "20180812_eevee.json",
+  "20180910_moltres.json",
+  "20180920_birds.json",
+  "20180922_meganium.json",
+  "20181021_beldum.json",
+  "20181023_mewtwo.json",
+  "20181110_cyndaquil.json",
+  "20190112_totadile.json",
+  "20190129_breloom.json",
+  "20190216_swinhub.json",
+  "20190323_treecko.json"
+];
+
+export default specials;
