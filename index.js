6c3a01681baa46079416f706f0712f89ee2e3135
Ethan <ethan.r.davidson@gmail.com>
2019-05-19 02:27:17 -0700

    its done, and its beautiful😍

---
+++ index.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+
+// Globals
+const port = 9000;
+
+// Main Process
+(() => {
+
+  const app = express();
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+  app.listen(port);
+  console.log(`Listening on port ${port}...`);
+
+})();
