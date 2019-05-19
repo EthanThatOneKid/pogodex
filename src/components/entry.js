6bfeaa0721a3226ee3495be54ba1cce23abeaff1
Ethan <ethan.r.davidson@gmail.com>
2019-05-19 02:03:37 -0700

    Update entry.js

--- src/components/entry.js
+++ src/components/entry.js
@@ -47,40 +47,44 @@ export default class Entry extends Component {
       });
     }
     return (
+
       <div className="entry" id={this.data.name} style={style.div}>
+
         <details>
+
           <summary style={style.summary} onClick={this.toggle}>
             <img className="icon" alt={this.data.name} src={this.data.icon}></img>
             {this.data.name}
             <Shine data={this.data.shiny}/>
           </summary>
-          <div>
 
-            <section>
+          <table><tbody align="center"><tr>
+            <td>
               {this.data.types.map(t => <Type data={t}/>)}
-            </section>
-
-            <section>
+            </td>
+          </tr><tr>
+            <td>
               <p>stats:</p>
               <Stats data={this.data.stats}/>
-            </section>
-
-            <section>
+            </td>
+          </tr><tr>
+            <td>
               <p>fast attacks:</p>
               {this.data.moves.fast.map(m => <Move data={m}/>)}
               <p>charge moves:</p>
               {this.data.moves.charge.map(m => <Move data={m}/>)}
-            </section>
-
-            <section>
+            </td>
+          </tr><tr>
+            <td>
               {(!!this.data.evolution.length) ? <p>evolution:</p> : <></>}
               {this.data.evolution.map(e => <EvolutionBranch data={e}/>)}
-            </section>
-
-          </div>
+            </td>
+          </tr></tbody></table>
 
         </details>
+
       </div>
+
     );
   }
 }
