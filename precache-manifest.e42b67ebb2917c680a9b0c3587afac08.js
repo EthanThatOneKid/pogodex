68380295ce68fc1050eaa8de1d140819b2960c9a
Ethan <ethan.r.davidson@gmail.com>
2019-07-26 00:22:08 -0700

    Updates

---
+++ precache-manifest.e42b67ebb2917c680a9b0c3587afac08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cdf5e80a6c3fa0c78df",
+    "url": "/pogodex/static/js/runtime~main.6f26ca8e.js"
+  },
+  {
+    "revision": "ab02ebb9b6c3305a023d",
+    "url": "/pogodex/static/js/main.5ae0bbb7.chunk.js"
+  },
+  {
+    "revision": "055b024aa280e633c455",
+    "url": "/pogodex/static/js/2.0f99328c.chunk.js"
+  },
+  {
+    "revision": "ab02ebb9b6c3305a023d",
+    "url": "/pogodex/static/css/main.bcf1a1b0.chunk.css"
+  },
+  {
+    "revision": "f429ce774bf75dabbdd122500321d8d9",
+    "url": "/pogodex/index.html"
+  }
+];
